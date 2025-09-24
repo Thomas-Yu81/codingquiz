e4ee57112b78ef8835139fa49ecb2df832681f10
apple <apple@appledeMacBook-Pro.local>
2025-09-24 14:27:41 +0800

    update review code

--- review.py
+++ review.py
@@ -6,6 +6,13 @@ def add_to_list(value, my_list=[]):
     my_list.append(value)
     return my_list
 
+print(add_to_list(5))
+print(add_to_list(6))
+
+the result is:
+[5]
+[5, 6]
+
 issue: my_list is mutable, can not use default [] as paramter, it should be None and then initialize inside the function. 
        default [] is created only once, when the function is defined, rather than creating a new instance each time the function is called. 
        so all calls to add_to_list which don't provide my_list will share the same list object, it will cause an unexpected accumulation of values.
@@ -42,6 +49,11 @@ this function has 2 issues with string formatting and coding standards.
 def format_greeting(name, age):
     return "Hello, my name is {name} and I am {age} years old."
 
+print(format_greeting("Will", 46))
+
+the result is:
+Hello, my name is {name} and I am {age} years old.
+
 issue 1: string formatting issue, it maybe need to use f strings
 issue 2: coding standard issee, usually use string + variabel
 """
@@ -61,8 +73,24 @@ class Counter:
         self.count += 1
     def get_count(self):
         return self.count
+
+
+c1 = Counter()
+print(c1.get_count()) 
+
+c2 = Counter()
+print(c2.get_count()) 
+
+c3 = Counter()
+print(c3.get_count())   
+the result is:
+1
+1
+1
+
 issue: the counter cannot continue counting through multiple calls, counter should use class variable to count the number of instances
 """
+
 class Counter:
     count = 0
     def __init__(self):
@@ -108,6 +136,9 @@ for _ in range(10):
 for t in threads:
     t.join()
 
+print(counter.count)
+the result may be less than 10000.
+
 issue: when multiple threads are running, they may all read the same old value, and then increment it, the result will be less than expected.
        we can use threading.Lock to ensure that only one thread can modify the count at a time.
 """
@@ -145,6 +176,11 @@ def count_occurrences(lst):
         else:
             counts[item] = 1
     return counts
+
+print(count_occurrences(['aaaa', 'bbbb', 'aaa', 'a', 'aaaa', 'bbbb']))
+the result is:
+{'aaaa': 1, 'bbbb': 1, 'aaa': 1, 'a': 1}
+
 issue: it may be the typo, the operator should be += , not =+
 """
 def count_occurrences(lst):
