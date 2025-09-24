bca727d8d55b9cf7be100fc54762350a30212ab6
apple <apple@appledeMacBook-Pro.local>
2025-09-24 14:20:39 +0800

    update review code

--- review.py
+++ review.py
@@ -1,9 +1,14 @@
 # Review 1
-""" this function has an issue with mutable default argument
+""" 
+this function has an issue with mutable default argument
 
 def add_to_list(value, my_list=[]):
     my_list.append(value)
     return my_list
+
+issue: my_list is mutable, can not use default [] as paramter, it should be None and then initialize inside the function. 
+       default [] is created only once, when the function is defined, rather than creating a new instance each time the function is called. 
+       so all calls to add_to_list which don't provide my_list will share the same list object, it will cause an unexpected accumulation of values.
 """
 def add_to_list(value, my_list=None):
     if my_list is None:
@@ -13,17 +18,32 @@ def add_to_list(value, my_list=None):
 
 value = 10
 my_list = [2, 2, 2]
-print(add_to_list(value, my_list))  
+
+# my_list is [2, 2, 2, 10]
+print(add_to_list(value, my_list))
+
+# my_list is [2, 2, 2, 10, 3]
+print(add_to_list(3, my_list))
+
+# my_list is [2, 2, 2, 10, 3, 4]
+print(add_to_list(4, my_list))
+
+# my_list is [5]
+print(add_to_list(5))
+
+# my_list is [6]
+print(add_to_list(6))
 
 
 # Review 2
-""" this function has 2 issues with string formatting and coding standards.
+""" 
+this function has 2 issues with string formatting and coding standards.
 
 def format_greeting(name, age):
     return "Hello, my name is {name} and I am {age} years old."
 
-issue 1: it maybe need to use f strings
-issue 2: usually use string + variabel
+issue 1: string formatting issue, it maybe need to use f strings
+issue 2: coding standard issee, usually use string + variabel
 """
 def format_greeting(name, age):
     # return f"Hello, my name is {name} and I am {age} years old."
@@ -32,7 +52,8 @@ def format_greeting(name, age):
 print(format_greeting("Will", 46))
 
 # Review 3
-""" this class has an issue with class variable vs instance variable
+""" 
+this class has an issue with class variable vs instance variable
 
 class Counter:
     count = 0
@@ -40,7 +61,7 @@ class Counter:
         self.count += 1
     def get_count(self):
         return self.count
-issue: counter should use class variable to count the number of instances
+issue: the counter cannot continue counting through multiple calls, counter should use class variable to count the number of instances
 """
 class Counter:
     count = 0
@@ -49,15 +70,23 @@ class Counter:
     def get_count(self):
         return Counter.count
 
+# Counter result is 1
 c1 = Counter()
-print(c1.get_count()) # Counter result is 1
+print(c1.get_count()) 
+
+# Counter result is 2
 c2 = Counter()
-print(c2.get_count()) # Counter result is 2
+print(c2.get_count()) 
+
+# Counter result is 3
 c3 = Counter()
-print(c3.get_count()) # Counter result is 3
+print(c3.get_count()) 
+
 
 # Review 4
-""" this function has an issue with thread safety
+""" 
+this function has an issue with thread safety
+
 import threading
 class SafeCounter:
     def __init__(self):
@@ -79,7 +108,8 @@ for _ in range(10):
 for t in threads:
     t.join()
 
-issue: the increment method is not thread safe.
+issue: when multiple threads are running, they may all read the same old value, and then increment it, the result will be less than expected.
+       we can use threading.Lock to ensure that only one thread can modify the count at a time.
 """
 import threading
 class SafeCounter:
@@ -103,10 +133,10 @@ for _ in range(10):
 for t in threads:
     t.join()
 
-print(counter.count)  # 10000
+print(counter.count)
 
 # Review 5
-""" this function has an issue with counting occurrences of elements in a list
+""" this function has an issue with operator
 def count_occurrences(lst):
     counts = {}
     for item in lst:
@@ -115,7 +145,7 @@ def count_occurrences(lst):
         else:
             counts[item] = 1
     return counts
-issue: it may be the typo, the operator should be += instead of =+
+issue: it may be the typo, the operator should be += , not =+
 """
 def count_occurrences(lst):
     counts = {}
@@ -126,5 +156,6 @@ def count_occurrences(lst):
             counts[item] = 1
     return counts
 
-print(count_occurrences(['aaaa', 'bbbb', 'aaa', 'a', 'aaaa', 'bbbb'])) 
 # {'aaaa': 2, 'bbbb': 2, 'aaa': 1, 'a': 1}
+print(count_occurrences(['aaaa', 'bbbb', 'aaa', 'a', 'aaaa', 'bbbb'])) 
+
